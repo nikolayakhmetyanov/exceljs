a115b8e276bc2e9a153cd43180c8d611ea1bee4f
Nikolay Akhmetyanov <nikolay.akhmetyanov@gmail.com>
2020-12-06 17:52:45 +0300

    add css-layout and other fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
 const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyPlugin = require("copy-webpack-plugin")
+const CopyPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const isProd = process.env.NODE_ENV === 'production'
 const isDev = !isProd
 
-const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
+const filename = (ext) => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
 
 const jsLoaders = () => {
   const loaders = [
     {
-      loader: "babel-loader",
+      loader: 'babel-loader',
       options: {
         presets: ['@babel/preset-env']
       }
@@ -29,7 +29,7 @@ const jsLoaders = () => {
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
-  entry: ["@babel/polyfill", './index.js'],
+  entry: ['@babel/polyfill', './index.js'],
   output: {
     filename: filename('js'),
     path: path.resolve(__dirname, 'dist')
@@ -41,7 +41,7 @@ module.exports = {
       '@core': path.resolve(__dirname, 'src/core')
     }
   },
-  devtool: isDev ?  'source-map' : false,
+  devtool: isDev ? 'source-map' : false,
   devServer: {
     port: 3000,
     hot: isDev
@@ -58,13 +58,13 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, "src/favicon.ico"),
-          to: path.resolve(__dirname, "dist"),
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist'),
         }
       ],
     }),
     new MiniCssExtractPlugin({
-      filename: filename("css")
+      filename: filename('css')
     })
   ],
   module: {
@@ -73,18 +73,14 @@ module.exports = {
         test: /\.s[ac]ss$/i,
         use: [
           {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              hmr: isDev,
-              reloadAll: true
-            }
+            loader: MiniCssExtractPlugin.loader
           },
-          "css-loader",
-          "sass-loader"
+          'css-loader',
+          'sass-loader'
         ],
       },
       {
-        test: /\.m?js$/,
+        test: /\.js$/,
         exclude: /node_modules/,
         use: jsLoaders()
       }
